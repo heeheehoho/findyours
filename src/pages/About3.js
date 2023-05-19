7dfef2e8f0040da21da41e28764d23b0a3632b16
희호 <heraok0153@gmail.com>
2023-05-19 14:24:45 +0900

    Add HTML created with D3
    Add HTML created with D3,

--- src/pages/About3.js
+++ src/pages/About3.js
@@ -1,13 +1,103 @@
-// src/pages/...
-import React from "react";
-import '../pages.css'
+import React, { useEffect, useRef } from "react";
+import { select, scaleLinear, scaleBand, axisBottom, axisLeft, axisRight } from "d3";
+import "../pages.css";
 
-class About3 extends React.Component {
-  render () {
-    return (
-        <h1 className='logo'>실종현황 페이지</h1>
-    )
-  }
-}
+const About3 = () => {
+  const svgRef = useRef(null);
 
-export default About3; 
+  useEffect(() => {
+    const sampleData = [
+      { name: 2016, value1: 67907, value2: 385 },
+      { name: 2017, value1: 65830, value2: 487 },
+      { name: 2018, value1: 75592, value2: 524 },
+      { name: 2019, value1: 75432, value2: 673 },
+      { name: 2020, value1: 67612, value2: 1178 },
+    ];
+
+    const width = 400;
+    const height = 300;
+    const margin = { top: 20, right: 40, bottom: 30, left: 60 };
+    const innerWidth = width - margin.left - margin.right;
+    const innerHeight = height - margin.top - margin.bottom;
+
+    const svg = select(svgRef.current)
+      .attr("width", width)
+      .attr("height", height);
+
+    const g = svg
+      .append("g")
+      .attr("transform", `translate(${margin.left}, ${margin.top})`);
+
+    const xScale = scaleBand()
+      .domain(["실종자 수", "미발견 실종자 수"])
+      .range([innerWidth / 4, (innerWidth / 4) * 3])
+      .padding(0.1);
+
+    const yScale = scaleLinear().domain([100000, 30000]).range([0, innerHeight]);
+
+    const y2Scale = scaleLinear().domain([1200, 300]).range([0, innerHeight]);
+
+    g.append("g")
+      .attr("transform", `translate(0, ${innerHeight})`)
+      .call(axisBottom(xScale));
+
+    g.append("g").call(axisLeft(yScale));
+
+    g.append("g")
+      .attr("transform", `translate(${innerWidth}, 0)`)
+      .call(axisRight(y2Scale));
+
+    function updateGraph(data) {
+      const bars1 = g.selectAll(".bar1").data([data], (d) => d.name);
+      const bars2 = g.selectAll(".bar2").data([data], (d) => d.name);
+
+      bars1
+        .enter()
+        .append("rect")
+        .attr("class", "bar1")
+        .attr("x", (d) => xScale("실종자 수") - xScale.bandwidth() / 2)
+        .attr("y", innerHeight)
+        .attr("width", xScale.bandwidth())
+        .attr("height", 0)
+        .merge(bars1)
+        .transition()
+        .duration(3000)
+        .attr("x", (d) => xScale("실종자 수") - xScale.bandwidth() / 2)
+        .attr("y", (d) => yScale(d.value1))
+        .attr("height", (d) => innerHeight - yScale(d.value1));
+
+      bars2
+        .enter()
+        .append("rect")
+        .attr("class", "bar2")
+        .attr("x", (d) => xScale("미발견 실종자 수") + xScale.bandwidth() / 2)
+        .attr("y", innerHeight)
+        .attr("width", xScale.bandwidth())
+        .attr("height", 0)
+        .merge(bars2)
+        .transition()
+        .duration(3000)
+        .attr("x", (d) => xScale("미발견 실종자 수") + xScale.bandwidth() / 2)
+        .attr("y", (d) => y2Scale(d.value2))
+        .attr("width", xScale.bandwidth())
+        .attr("height", (d) => innerHeight - y2Scale(d.value2));
+    }
+
+    let currentIndex = 0;
+    setInterval(() => {
+      currentIndex = (currentIndex + 1) % sampleData.length;
+      updateGraph(sampleData[currentIndex]);
+    }, 3000);
+  }, []);
+
+  return (
+    <div>
+      <h1 className="logo">실종현황 페이지</h1>
+      <svg ref={svgRef} width="500px" height="500px">
+        <g id="chartContainer"></g>
+      </svg>
+    </div>
+  );
+};
+
+export default About3;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,16 +1,71 @@
 import React from "react";
 import tree from '../images/tree.jpg';
-import intro from '../images/slide1.png';
+import main1 from '../images/main1.jpeg';
+import main2 from '../images/main2.jpeg';
+import main3 from '../images/main3.jpeg';
+import "./Home.css";
 
 class Home extends React.Component {
-  render () {
-    return (          
-      <body className="homebody">
-      <h1 className='logo'>Find Yours</h1>
-      <img id="backimg" src={tree} alt="tree" />
-      <img src={intro} alt="introduce" />
-    </body>     
-    )
+  constructor(props) {
+    super(props);
+    this.state = {
+      isScrolled: false,
+    };
+    this.scrollRef = React.createRef();
+  }
+
+  componentDidMount() {
+    window.addEventListener('scroll', this.handleScroll);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('scroll', this.handleScroll);
+  }
+
+  handleScroll = () => {
+    const scrollPos = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
+    const scrollRefPos = this.scrollRef.current.getBoundingClientRect().top + scrollPos;
+    const windowHeight = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight || 0;
+
+    if (scrollPos >= scrollRefPos - windowHeight / 2) {
+      this.setState({ isScrolled: true });
+    } else {
+      this.setState({ isScrolled: false });
+    }
+  };
+
+  render() {
+    const { isScrolled } = this.state;
+
+    return (
+      <div>
+        <div className={`section ${isScrolled ? 'is-scrolled' : ''}`}>
+          <img src={tree} alt="Tree" className="background-image" />
+        </div>
+        <div className="section" ref={this.scrollRef}>
+          <div className="image-container">
+            <div className={`image-overlay ${isScrolled ? 'active' : ''}`}>
+              <img src={main1} alt="Main 1" />
+              <div className="image-caption">
+                <h2>실종자 모습</h2>
+              </div>
+            </div>
+            <div className={`image-overlay ${isScrolled ? 'active' : ''}`}>
+              <img src={main2} alt="Main 2" />
+              <div className="image-caption">
+                <h2>실시간 탐지</h2>
+              </div>
+            </div>
+            <div className={`image-overlay ${isScrolled ? 'active' : ''}`}>
+              <img src={main3} alt="Main 3" />
+              <div className="image-caption">
+                <h2>실종현황</h2>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 
